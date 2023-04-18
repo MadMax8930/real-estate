58c3abd55091396bc024a2274522c1b6de5a5780
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-18 17:34:18 +0200

    final

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
    const { data } = await axios.get(url, {
       headers: {
-         'x-rapidapi-key': 'e63c9011cdmshb275bdd29b3dce1p1c9099jsn2a548a1316f1',
+         'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
          'x-rapidapi-host': 'bayut.p.rapidapi.com'
       }
    });
