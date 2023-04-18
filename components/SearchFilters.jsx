f16e19e557fac73a7dcd9ade9f368f24cf119141
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-18 14:53:53 +0200

    search filtering final

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -7,9 +7,21 @@ import { filterData, getFilterValues } from '../utils/filterData';
 
 const SearchFilters = () => {
    const [filters, setFilters] = useState(filterData);
-   const searchProperties = (filterValues) => {
+   const router = useRouter();
 
-   }
+   const searchProperties = (filterValues) => {
+      const path = router.pathname;
+      const { query } = router;
+
+      const values = getFilterValues(filterValues);
+      values.forEach((item) => {
+         if(item.value && filterValues?.[item.name]) {
+            query[item.name] = item.value;
+         }
+      })
+
+      router.push({ pathname: path, query: query });
+   };
 
    return (
       <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
